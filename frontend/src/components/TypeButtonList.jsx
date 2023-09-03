8d5a03c6039ff94f20ff8a0d80e2316e79ab7e3e
Jackie <jackie15910@gmail.com>
2023-09-03 18:46:38 -0400

    Cleared and moved typebuttonlist

--- frontend/src/components/TypeButtonList.jsx
+++ frontend/src/components/TypeButtonList.jsx
@@ -2,43 +2,15 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import TypeButton from './TypeButton';
 
-const TypeButtonList = () => {
-  
-  const [types, setTypes] = useState([]);
-  const [selectedTypes, setSelectedTypes] = useState([]);
+const TypeButtonList = (types, onTypeSelect) => {
 
-  const onTypeSelect = (typeName) => {
-    if (selectedTypes.includes(typeName)) {
-      setSelectedTypes(selectedTypes.filter((type) => type !== typeName)); //Filters out any existing types
-    } else {
-      console.log('Selected type:', typeName);
-      setSelectedTypes([...selectedTypes, typeName]);
-    }
-  }
-
-  //This will show all the currently selected types
-  useEffect(() => {
-    console.log('Selected types:', selectedTypes);
-  }, [selectedTypes]);
-
-  useEffect(() => {
-    axios
-      .get('https://pokeapi.co/api/v2/type')
-      .then((response) => {
-        setTypes(response.data.results);
-      })
-      .catch((error) => {
-        console.error('Error fetching Pokémon types data:', error);
-      });
-  }, []);
-
-  if (types.length === 0) {
+  if (typesData.length === 0) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
-      {types.map((type) => (
+      {typesData.map((type) => (
         <TypeButton
           key={type.name}
           typeName={type.name}
