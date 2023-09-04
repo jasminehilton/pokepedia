4775f28a6fc7a9fe53330fde31d34f2496de31dd
Mosse-Sox <autumngreenie990@gmail.com>
2023-09-04 15:37:04 -0600

    removes unused code from app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
 
 import PokemonInfo from "./components/PokemonInfo";
-import usePokeDetails from "./hooks/usePokeDetails";
-import PokemonDetails from "./components/pokemonDetails";
+
 // import useCollections from './hooks/useCollections';
 
 function App() {
-  const { pokemonDetails, pokemonLocations, pokemonWeaknesses } = usePokeDetails(200);
-  // console.log(pokemonWeaknesses);
   return (
     <div className="App">
       {/* <PokemonInfo /> */}
-      <PokemonDetails pokemon={pokemonDetails} locations={pokemonLocations} />
     </div>
   );
 }
