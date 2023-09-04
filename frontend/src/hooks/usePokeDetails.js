50c4188538168a9ffca54d3cdf9c123cd87313b0
Mosse-Sox <autumngreenie990@gmail.com>
2023-09-04 15:14:00 -0600

    starts changing usePokeDetails.js into a helper function that just returns the encounters

--- frontend/src/hooks/usePokeDetails.js
+++ frontend/src/hooks/usePokeDetails.js
@@ -1,53 +1,16 @@
-import { useEffect, useState } from "react";
-import P from "../helpers/pokeApiConfig";
-
-const usePokeDetails = (pokemon_id) => {
-  const [pokemonDetails, setPokemonDetails] = useState(null);
-  const [pokemonLocations, setPokemonLocations] = useState(null);
-  const [ pokemonWeaknesses, setPokemonWeaknesses] = useState(null);
-
-  useEffect(() => {
-    const fetchPokemonDetails = async () => {
-      try {
-        const response = await P.getPokemonByName(pokemon_id);
-        setPokemonDetails(response);
-      } catch (error) {
-        console.error("Error fetching pokemon details:", error);
-      }
-    };
-
-    const fetchPokemonEncounters = async () => {
-      try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_id}/encounters`);
-        const data = await response.json();
-        setPokemonLocations(data);
-      } catch (error) {
-        console.error("Error fetching encounter details:", error);
-      }
-    };
-
-    // const fetchPokemonWeaknesses = async () => {
-    //   try {
-    //     const response = await fetch(`https://pokeapi.co/api/v2/type/${}`);
-    //     const data = await response.json();
-    //     setPokemonLocations(data);
-    //   } catch (error) {
-    //     console.error("Error fetching encounter details:", error);
-    //   }
-    // };
-
-    if (pokemon_id) {
-      fetchPokemonDetails();
-      fetchPokemonEncounters();
-      // fetchPokemonWeaknesses();
-    }
-  }, [pokemon_id]);
-
-  return {
-    pokemonDetails,
-    pokemonLocations,
-    pokemonWeaknesses
-  };
+const fetchPokemonEncounters = async (dispatch, pokemon_id) => {
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemon_id}/encounters`
+    );
+    const data = await response.json();
+    dispatch({
+      type: "FETCH_POKEMON_LOCATIONS",
+      payload: data,
+    });
+  } catch (error) {
+    console.error("Error fetching encounter details:", error);
+  }
 };
 
-export default usePokeDetails;
+export default fetchPokemonEncounters;
