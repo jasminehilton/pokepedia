35ba144cdf816087095ddef736a1e743fe91c11f
Reyna May <roma.reynamayA@gmail.com>
2023-09-05 16:40:20 -0600

    Clean up the code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
 import "./App.css";
-import { PokemonDataProvider, usePokemonDataContext, usePokemonDataDispatchContext } from "./providers/pokeProvider.js";
 import HomeRoute from "./routes/HomeRoute";
 
-import PokemonModal from "./routes/PokemonModal";
-
 function App() {
-  const state = usePokemonDataContext();
-
 
   return (
     <div>
       <HomeRoute />
-      {/* {state.isModalVisible && <PokemonModal />} */}
     </div>
   );
 }
