6d2108d901b012a6b950308c4e51ce8f12a7ab63
Reyna May <roma.reynamayA@gmail.com>
2023-09-06 20:22:52 -0600

    Create a footer component

---
+++ frontend/src/components/PokemonFooter.jsx
@@ -0,0 +1,13 @@
+// create a footer
+const PokemonFooter = () => {
+  return (
+    <footer className="pokemon-footer">
+      <div className="pokemon-footer__container">
+        <span className="text-muted">Pokemon Footer</span>
+        <p>&copy; 2023 Wassup yo!</p>
+      </div>
+    </footer>
+  );
+}
+
+export default PokemonFooter;
