6ebbc42ec5f5baa74c2907393b663ad68ed59675
Mosse-Sox <autumngreenie990@gmail.com>
2023-09-06 21:19:02 -0600

    fixes pokemon list

--- frontend/src/components/PokemonList.jsx
+++ frontend/src/components/PokemonList.jsx
@@ -1,39 +1,27 @@
 import React, { useEffect } from "react";
 import fetchPokemonData from "../helpers/fetchPokemonData";
-import { usePokemonDataContext, usePokemonDataDispatchContext } from "../providers/pokeProvider";
+import {
+  usePokemonDataContext,
+  usePokemonDataDispatchContext,
+} from "../providers/pokeProvider";
 import PokemonModal from "../routes/PokemonModal";
 import PokemonListItem from "./PokemonListItem";
 import Pagination from "./Pagination";
 import handlePageChange from "../helpers/handlePageChange";
-<<<<<<< HEAD
-import PokemonLogo from "./PokemonLogo"
-=======
-import getDisplayedPokemon from "../helpers/getDisplayedPokemon";
-
->>>>>>> feature/filtering-pokemon-data-by-region-integration
+import PokemonLogo from "./PokemonLogo";
 
 const PokemonList = ({ isOpen, onClose }) => {
   const state = usePokemonDataContext();
   const dispatch = usePokemonDataDispatchContext();
 
   const onDisplayPokemonModal = (pokemon) => {
-    dispatch({ type: 'DISPLAY_POKEMON_DATA', payload: pokemon });
+    dispatch({ type: "DISPLAY_POKEMON_DATA", payload: pokemon });
   };
 
   useEffect(() => {
     fetchPokemonData(dispatch);
   }, []);
-<<<<<<< HEAD
 
-  // getDisplayedPokemon(state.pokemonData, state.filteredPokemonData, state.currentPage, state.itemsPerPage, dispatch);
-  useEffect(() => {
-    getDisplayedPokemon(state.pokemonData, state.filteredPokemonData, state.currentPage, state.itemsPerPage, dispatch);
-  }, [state.filteredPokemonData, state.pokemonData])
-
-
-=======
- 
->>>>>>> feature/filtering-pokemon-data-by-region-integration
   return (
     <div>
       {state.isLoading ? (
@@ -41,8 +29,25 @@ const PokemonList = ({ isOpen, onClose }) => {
       ) : state.error ? (
         <p>Error: {state.error}</p>
       ) : (
-        <div className="homepage-container">
-          <Pagination next={() => handlePageChange(dispatch, state.currentPage + 1, state.itemsPerPage, state.pokemonData.length)} prev={() => handlePageChange(dispatch, state.currentPage - 1, state.itemsPerPage, state.pokemonData.length)} />
+        <div>
+          <Pagination
+            next={() =>
+              handlePageChange(
+                dispatch,
+                state.currentPage + 1,
+                state.itemsPerPage,
+                state.pokemonData.length
+              )
+            }
+            prev={() =>
+              handlePageChange(
+                dispatch,
+                state.currentPage - 1,
+                state.itemsPerPage,
+                state.pokemonData.length
+              )
+            }
+          />
           <PokemonLogo />
           <div className="pokemon-container">
             {state.displayedPokemon.map((pokemon, index) => (
@@ -55,9 +60,7 @@ const PokemonList = ({ isOpen, onClose }) => {
           </div>
         </div>
       )}
-      {state.isModalVisible && (
-        <PokemonModal />
-      )}
+      {state.isModalVisible && <PokemonModal />}
     </div>
   );
 };
