0d0a327471cce3f608748bfde1bd437f81b4ea2b
Mosse-Sox <autumngreenie990@gmail.com>
2023-09-06 20:07:05 -0600

    adds a new helper for fetching all pokemon in a region

--- frontend/src/components/RegionList.jsx
+++ frontend/src/components/RegionList.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Pokedex from "pokedex-promise-v2";
-import axios from "axios";
 import RegionListItem from "./RegionListItem";
 import { usePokemonDataContext, usePokemonDataDispatchContext} from '../providers/pokeProvider';
 import getRegions from '../helpers/fetchRegions';
 import setSelectedRegion from "../helpers/setSelectedRegion";
+import getPokemonsByRegion from "../helpers/fetchPokemonByRegion";
 
 const P = new Pokedex();
 
@@ -12,44 +12,16 @@ function RegionList() {
 	const state = usePokemonDataContext(); //imports the state
   const dispatch = usePokemonDataDispatchContext(); //imports dispatch
 
-  
-
-  // gets the list of regions 
-	
-
-  // // gets the pokemon associated with the selected region
-  // const getPokemonsByRegion = () => { 
-  //   let allPokemons = []
-  //   axios.get(selectedRegion.url)
-  //   .then((response) => {
-  //     console.log('response ', response.data)
-  //     let pokedexes = response.data.pokedexes
-  //     // iterates through the list of pokedexes in the region object
-  //     for(let pokedex of pokedexes) {
-  //       axios.get(pokedex.url)
-  //       .then((pokedexResponse) => {
-  //         let pokemons = pokedexResponse.data.pokemon_entries.map((pokemonEntry) => {
-  //           return pokemonEntry.pokemon_species
-  //         })
-  //         setPokemonByRegion(allPokemons.concat(pokemons))
-  //       })
-  //     }
-  //   }).catch((error) => {
-  //     console.log('error ', error)
-  //   })
-  // }
-
-	// on page load get all the regions
 	useEffect(() => {
 		console.log("load the pokemon regions now");
 		getRegions(P, dispatch,);
 	}, []);
 
-  // useEffect(() => {
-  //   if(selectedRegion) {
-  //     getPokemonsByRegion()
-  //   }
-  // }, [selectedRegion])
+  useEffect(() => {
+    if(state.filters.regions) {
+      getPokemonsByRegion(state.filters.regions, dispatch);
+    }
+  }, [state.filters.regions])
 
 	return (
 
--- frontend/src/helpers/setSelectedRegion.js
+++ frontend/src/helpers/setSelectedRegion.js
@@ -2,7 +2,7 @@ const setSelectedRegion = (region, dispatch, selectedRegion) => {
   if (selectedRegion === region.name) {
     dispatch({type: 'ADD_REGION_FILTER', payload: ''});
   } else {
-    dispatch({type: 'ADD_REGION_FILTER', payload: region.name});
+    dispatch({type: 'ADD_REGION_FILTER', payload: region});
   }
 };
 
