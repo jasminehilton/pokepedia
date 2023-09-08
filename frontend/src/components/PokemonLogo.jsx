ce24ef55a83b6321b4b09373f60b2a612eccfe87
Reyna May <roma.reynamayA@gmail.com>
2023-09-08 16:45:04 -0600

    Remove effects on logo

--- frontend/src/components/PokemonLogo.jsx
+++ frontend/src/components/PokemonLogo.jsx
@@ -5,7 +5,7 @@ const PokemonLogo = () => {
     <div className="pokemonLogoContainer">
     <img
       className="pokemon-logo"
-      src="https://fontmeme.com/permalink/230908/d9ac56c5127687a6a23707f77cca5f56.png"
+      src="https://fontmeme.com/permalink/230908/e528326b2c0a56e5899b238abfcec565.png"
       alt="pokemon-logo"
     />
     </div>
