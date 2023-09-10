840eb5a817d95508d5aab9461fcf12128b1bbc92
Reyna May <roma.reynamayA@gmail.com>
2023-09-09 19:08:27 -0600

    Adjust the style of the modal

--- frontend/src/routes/RegistrationModal.jsx
+++ frontend/src/routes/RegistrationModal.jsx
@@ -6,12 +6,12 @@ import Registration from "../components/Registration";
 const RegistrationModal = ({ showRegistration, toggleModal }) => {
   return (
     <div className={`registration-modal ${showRegistration ? "active" : ""}`}>
-      <div className="modal-content">
+      <div >
         <span className="form-close" onClick={toggleModal}>
           O
         </span>
 
-        <Registration />
+        <Registration className="modal-content"/>
       </div>
     </div>
   );
