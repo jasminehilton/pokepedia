29f118dbe6d7f9bbfdce9291d8dd125520b1cc70
Reyna May <roma.reynamayA@gmail.com>
2023-09-11 17:57:10 -0600

    Add greyed-out caught-shiny

--- frontend/src/components/ShinyIcon.jsx
+++ frontend/src/components/ShinyIcon.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../styles/icons.css";
+import uncaughtShiny from "../icons/uncaught-shiny.png"
 
 const ShinyIcon = () => {
   return (
     <div>
       <img className="shiny-icon"
-        src="https://img.icons8.com/bubbles/50/pixel-star.png"
+        src={uncaughtShiny}
+        // SHINY ONE COMMENTED OUT
+        // src={"https://img.icons8.com/bubbles/50/pixel-star.png"}
         alt="pixel-star" />
     </div>
   );
