67f3ee8b3e638338e344f3acc3379888e3c5e012
Jackie <jackie15910@gmail.com>
2023-09-12 22:07:25 -0400

    Fixed 3000

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(morgan(ENVIRONMENT));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3002');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
